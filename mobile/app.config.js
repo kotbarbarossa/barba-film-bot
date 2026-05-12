567eb6f3aa23b1b36ee834c5db718fdfea384939
Eldar Barbarossa <ultra.kot.dev@gmail.com>
2026-05-13 00:33:22 +0700

    chore(mobile): bump versionCode to 3 for Play Store release

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -29,7 +29,7 @@ module.exports = {
       backgroundColor: '#faf7f2',
     },
     package: 'com.barbarossa.flickbook',
-    versionCode: 2,
+    versionCode: 3,
   },
   web: {
     bundler: 'metro',
